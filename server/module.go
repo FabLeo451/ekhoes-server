1090723dca24869371b8cbae400da36f6d215889
FabLeo451 <fab.leo@gmail.com>
2026-04-01 16:56:18 +0200

    Introducing modules

--- server/module.go
+++ server/module.go
@@ -3,22 +3,22 @@ package server
 import (
 	"ekhoes-server/herenow"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
 
 type Module struct {
-	Id       string
 	Name     string
 	InitFunc func(*chi.Mux) error
 }
 
-var modules []Module
+var modules map[string]Module
 
 func init() {
-	modules = []Module{
-		{
-			Id:       "herenow",
+	modules = map[string]Module{
+		"herenow": {
 			Name:     "HereNow",
 			InitFunc: herenow.Init,
 		},
@@ -26,13 +26,20 @@ func init() {
 }
 
 func InitModules(r *chi.Mux) {
-	for _, m := range modules {
+	modulesEnv := os.Getenv("MODULES")
+
+	if modulesEnv == "" {
+		return
+	}
+
+	ids := strings.Split(modulesEnv, ",")
+
+	for _, id := range ids {
+		id = strings.TrimSpace(id)
+
+		m := modules[id]
 
 		log.Printf("Initializing module %s...", m.Name)
-
-		if m.InitFunc == nil {
-			continue;
-		}
 
 		if err := m.InitFunc(r); err != nil {
 			panic(err)
